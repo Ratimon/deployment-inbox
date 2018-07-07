86d1a171a3877eae9134b80d110c9f1164de18ae
Rati Montreewat <ratixoxo@gmail.com>
2018-07-07 23:31:18 +0800

    Verifying initial message test

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,75 +1,106 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-//import constructor function so it is capitl letter
-const web3 = new Web3(ganache.provider());
+//import constructor function so it is capital letter
+
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
 const { interface, bytecode } = require('../compile');
 
-// let accounts;
-// let inbox;
+let accounts;
+let inbox;
+// Const INITIAL_STRING = 'Hi there!'
+// For testing, a good way to make the test arguments inmmutable
 
-// beforeEach(async () => {
-//   // Get a list of all accounts
-//   accounts = await web3.eth.getAccounts();
+beforeEach(async () => {
+  // The word “async” before a function means one simple thing: a function always returns a promise.
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+  // The keyword await makes JavaScript wait until that promise settles and returns its result.
 
-//   // Use one of those accounts to deploy
-//   // the contract
-//   inbox = await new web3.eth.Contract(JSON.parse(interface))
-//     .deploy({
-//       data: bytecode,
-//       arguments: ['Hi there!']
-//     })
-//     .send({ from: accounts[0], gas: '1000000' });
-// });
+  // Use one of those accounts to deploy that can then be deployed
+  // the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+  // reresents what exists in blockchain
+    .deploy({
+      data: bytecode,
+      arguments: ['Hi there!']
+    })// does not actually deploy, it simply create a object
+    .send({ from: accounts[0], gas: '1000000' });
 
-// describe('Inbox', () => {
-//   it('deploys a contract', () => {
-//     assert.ok(inbox.options.address);
-//   });
+  inbox.setProvider(provider)  
+});
 
-//   it('has a default message', async () => {
-//     const message = await inbox.methods.message().call();
-//     assert.equal(message, 'Hi there!');
-//   });
+describe('Inbox', () => {
+  it('deploys a contract', () => {
+    assert.ok(inbox.options.address);
+
+    // console.log(inbox);
+    // Initially we want to see it works
+  });
+
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    //message() custommize the list of arguements, call() how such a fumction get called
+    assert.equal(message, 'Hi there!');
+  });
 
 //   it('can change the message', async () => {
 //     await inbox.methods.setMessage('bye').send({ from: accounts[0] });
 //     const message = await inbox.methods.message().call();
 //     assert.equal(message, 'bye');
 //   });
-// });
-
+});
 
 
 //////////////////////
-class Car {
-    park() {
-        return 'stopped';
-    }
+// beforeEach(()=> {
+//     // Get a list of all accounts
+//     web3.eth.getAccounts().then(fetchedAccounts => {
+//         console.log(fetchedAccounts);
+//     });
 
-    drive() {
-        return 'vroom'
-    }
-}
+//     //use one of those accounts to deploy
+//     //the contract
+// });
 
-//const car
-// car is not constant, we are going reassign the value
-let car;
+// describe('Inbox',()=> {
+//     it('deploys a contract',()=> {
 
-beforeEach(()=>{
-    // const car = new Car();
-    // since scope of car is not avaiable for descibe
-    car = new Car();
-});
+//     });
+// });
 
-describe('Car', () => {
-    it('can park', ()=> {
-        // const car = new Car();
-        assert.equal(car.park(), 'stopped')
-    });
 
-    it('can drive', ()=> {
-        // const car = new Car();
-        assert.equal(car.drive(), 'vroom')
-    });   
-});
+//////////////////////
+// class Car {
+//     park() {
+//         return 'stopped';
+//     }
+
+//     drive() {
+//         return 'vroom'
+//     }
+// }
+
+// //const car
+// // car is not constant, we are going reassign the value
+// let car;
+
+// beforeEach(()=>{
+//     // const car = new Car();
+//     // since scope of car is not avaiable for descibe
+//     car = new Car();
+// });
+
+// describe('Car', () => {
+//     it('can park', ()=> {
+//         // const car = new Car();
+//         assert.equal(car.park(), 'stopped')
+//     });
+
+//     it('can drive', ()=> {
+//         // const car = new Car();
+//         assert.equal(car.drive(), 'vroom')
+//     });   
+// });
